30aff1cfba4f9dc07c27a3344d7485ecbb3eb778
hcpatikadev <138597087+hcpatikadev@users.noreply.github.com>
2023-07-04 17:12:42 +0300

    Update odev1.c

--- odev1.c
+++ odev1.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+#include <string.h>
+
+void ReverseText(char *text) {
+    int i, j;
+    char temp;
+    int length = strlen(text);
+
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        temp = text[i];
+        text[i] = text[j];
+        text[j] = temp;
+    }
+}
+
+int main() {
+    char text[100];
+
+    printf("Metni girin: ");
+    gets(text);
+
+    ReverseText(text);
+
+    printf("Tersine çevrilmiş metin: %s\n", text);
+
+    return 0;
+}
